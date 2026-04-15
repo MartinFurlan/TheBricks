c3a20f398d659f1c0643769ee7386f9e4348289b
MartinFurlan <167509832+MartinFurlan@users.noreply.github.com>
2026-04-15 15:56:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,75 +1,41 @@
-var x = 150;
-var y = 200;
-var dx = 2;
-var dy = 4;
-var WIDTH;
-var HEIGHT;
-var r = 12;
-var f = 0;
-var ctx;
-var intervalId;
-var timerId;
-var level = 1;
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var actionBtn = document.getElementById("actionBtn");
+var scoreEl = document.getElementById("score");
+var hiddenScoreEl = document.getElementById("tocke");
+var timeEl = document.getElementById("cas");
+var levelEl = document.getElementById("level");
 
-var paddlecolor = "#0f2742";
-var ballcolor = "#fb8500";
-var brickcolors = {
-    1: "#7cc576",
-    2: "#f5a623",
-    3: "#d64541"
-};
-var start = true;
-var tocke;
-var sekunde;
-var izpisTimer;
+var x = 150, y = 200, dx = 2.8, dy = -2.8;
+var WIDTH = canvas.width, HEIGHT = canvas.height, r = 12;
 
-var paddlex;
-var paddleh;
-var paddlew;
-
-var rightDown = false;
-var leftDown = false;
-
-var bricks;
-var NROWS;
-var NCOLS;
-var BRICKWIDTH;
-var BRICKHEIGHT;
-var PADDING;
-var gameWon = false;
-
-var lives = 3;
-var controlsAdded = false;
-var BRICKOFFSETLEFT = 18;
-var BRICKOFFSETTOP = 42;
+var intervalId = null, timerId = null;
+var level = 1, score = 0, seconds = 0;
+var paddlex = 0, paddleh = 28, paddlew = 140;
+var rightDown = false, leftDown = false;
+var bricks = [], NROWS = 3, NCOLS = 4, BRICKWIDTH = 0, BRICKHEIGHT = 24, PADDING = 10;
+var BRICKOFFSETLEFT = 18, BRICKOFFSETTOP = 42;
+var gameWon = false, isPaused = false, gameStarted = false, assetsReady = 0;
 
 var paddleImg = new Image();
 var puckImg = new Image();
-var imagesReady = 0;
-
-var isPaused = false;
-var gameStarted = false;
-
-paddleImg.onload = imageLoaded;
-puckImg.onload = imageLoaded;
-
-paddleImg.onerror = function () {
-    console.log("Image not found: paddle.png");
-};
-
-puckImg.onerror = function () {
-    console.log("Image not found: puck.png");
-};
-
 paddleImg.src = "paddle.png";
 puckImg.src = "puck.png";
 
-function showGameAlert(title, text, icon, confirmText) {
+paddleImg.onload = assetLoaded;
+puckImg.onload = assetLoaded;
+
+function assetLoaded() {
+    assetsReady++;
+    if (assetsReady >= 2) resetBoard();
+}
+
+function showGameAlert(title, text, icon, confirmButtonText) {
     return Swal.fire({
         title: title,
         text: text,
         icon: icon,
-        confirmButtonText: confirmText,
+        confirmButtonText: confirmButtonText,
         background: "#091729",
         color: "#eef8ff",
         confirmButtonColor: "#ffffff",
@@ -84,37 +50,23 @@ function showGameAlert(title, text, icon, confirmText) {
     });
 }
 
-function imageLoaded() {
-    imagesReady++;
-    if (imagesReady >= 2) {
-        resetBoard();
-    }
+function formatTime(sec) {
+    var m = Math.floor(sec / 60);
+    var s = sec % 60;
+    if (m < 10) m = "0" + m;
+    if (s < 10) s = "0" + s;
+    return m + ":" + s;
 }
 
-function updateLevelDisplay() {
-    $("#level").html(level);
-}
+function updateUI() {
+    scoreEl.textContent = score;
+    hiddenScoreEl.textContent = score;
+    timeEl.textContent = formatTime(seconds);
+    levelEl.textContent = level;
 
-function updateScoreDisplay() {
-    $("#tocke").html(tocke);
-    $("#score").html(tocke);
-}
-
-function updateLivesDisplay() {
-    $("#lives").html(lives);
-}
-
-function updateActionButton() {
-    var btn = document.getElementById("actionBtn");
-    if (!btn) return;
-
-    if (!gameStarted) {
-        btn.textContent = "Start Game";
-    } else if (isPaused) {
-        btn.textContent = "Resume";
-    } else {
-        btn.textContent = "Pause";
-    }
+    if (!gameStarted) actionBtn.textContent = "Start Game";
+    else if (isPaused) actionBtn.textContent = "Resume";
+    else actionBtn.textContent = "Pause";
 }
 
 function stopLoops() {
@@ -127,7 +79,12 @@ function stopLoops() {
 function startLoops() {
     stopLoops();
     intervalId = setInterval(draw, 10);
-    timerId = setInterval(timer, 1000);
+    timerId = setInterval(function () {
+        if (!isPaused) {
+            seconds++;
+            timeEl.textContent = formatTime(seconds);
+        }
+    }, 1000);
 }
 
 function resetBall() {
@@ -135,177 +92,69 @@ function resetBall() {
     y = HEIGHT - 75;
     dx = 2.8;
     dy = -2.8;
-    start = true;
 }
 
-function init() {
-    ctx = $("#canvas")[0].getContext("2d");
-    WIDTH = $("#canvas").width();
-    HEIGHT = $("#canvas").height();
-
-    stopLoops();
-
-    resetBall();
-    init_paddle();
-    initbricks();
-
-    sekunde = 0;
-    izpisTimer = "00:00";
-    tocke = 0;
-    lives = 3;
-    gameWon = false;
-    isPaused = false;
-    gameStarted = true;
-
-    updateLevelDisplay();
-    $("#cas").html(izpisTimer);
-    updateScoreDisplay();
-    updateLivesDisplay();
-    updateActionButton();
-
-    draw();
-    startLoops();
-}
-
-function startNewGame() {
-    level = 1;
-    init();
-}
-
-function init_paddle() {
+function initPaddle() {
     paddlex = WIDTH / 2.5;
-    paddleh = 28;
     paddlew = 140;
 }
 
-function initbricks() {
+function initBricks() {
     if (level == 1) {
         NROWS = 3;
         NCOLS = 4;
     } else if (level == 2) {
         NROWS = 4;
         NCOLS = 6;
-    } else if (level == 3) {
-        NROWS = 5;
-        NCOLS = 8;
     } else {
         NROWS = 5;
         NCOLS = 8;
     }
 
-    PADDING = 10;
     BRICKWIDTH = Math.floor((WIDTH - (BRICKOFFSETLEFT * 2) - ((NCOLS - 1) * PADDING)) / NCOLS);
-    BRICKHEIGHT = 24;
-    bricks = new Array(NROWS);
+    bricks = [];
 
     for (var i = 0; i < NROWS; i++) {
-        bricks[i] = new Array(NCOLS);
+        bricks[i] = [];
         for (var j = 0; j < NCOLS; j++) {
             bricks[i][j] = 1;
         }
     }
 }
 
-function circle(x, y, r) {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.fill();
-}
-
-function rect(x, y, w, h) {
-    ctx.beginPath();
-    if (typeof ctx.roundRect === "function") {
-        ctx.roundRect(x, y, w, h, 6);
-    } else {
-        ctx.rect(x, y, w, h);
-    }
-    ctx.closePath();
-    ctx.fill();
-}
-
-function clear() {
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-}
-
 function allBricksDestroyed() {
     for (var i = 0; i < NROWS; i++) {
         for (var j = 0; j < NCOLS; j++) {
-            if (bricks[i][j] > 0) {
-                return false;
-            }
+            if (bricks[i][j] > 0) return false;
         }
     }
     return true;
 }
 
-function timer() {
-    var sekundeI;
-    var minuteI;
+function nextLevelCheck() {
+    if (!allBricksDestroyed() || gameWon) return;
 
-    if (start == true && !isPaused) {
-        sekunde++;
-        sekundeI = ((sekundeI = (sekunde % 60)) > 9) ? sekundeI : "0" + sekundeI;
-        minuteI = ((minuteI = Math.floor(sekunde / 60)) > 9) ? minuteI : "0" + minuteI;
-        izpisTimer = minuteI + ":" + sekundeI;
-        $("#cas").html(izpisTimer);
-    }
-}
+    gameWon = true;
+    stopLoops();
 
-function onKeyDown(evt) {
-    if (evt.keyCode == 39) {
-        rightDown = true;
-        evt.preventDefault();
-    } else if (evt.keyCode == 37) {
-        leftDown = true;
-        evt.preventDefault();
-    }
-}
-
-function onKeyUp(evt) {
-    if (evt.keyCode == 39) {
-        rightDown = false;
-        evt.preventDefault();
-    } else if (evt.keyCode == 37) {
-        leftDown = false;
-        evt.preventDefault();
-    }
-}
-
-function preveriZmago() {
-    if (!gameWon && allBricksDestroyed()) {
-        gameWon = true;
-        stopLoops();
-
-        if (level < 3) {
-            showGameAlert(
-                "Nice!",
-                "You completed level " + level + ".",
-                "success",
-                "Continue"
-            ).then(function () {
+    if (level < 3) {
+        showGameAlert("Nice!", "You completed level " + level + ".", "success", "Continue")
+            .then(function () {
                 level++;
                 gameWon = false;
                 isPaused = false;
-                updateLevelDisplay();
-                updateActionButton();
-
                 resetBall();
-                init_paddle();
-                initbricks();
+                initPaddle();
+                initBricks();
+                updateUI();
                 draw();
                 startLoops();
             });
-        } else {
-            showGameAlert(
-                "Champion!",
-                "You completed all levels.",
-                "success",
-                "Play Again"
-            ).then(function () {
+    } else {
+        showGameAlert("Champion!", "You completed all levels.", "success", "Play Again")
+            .then(function () {
                 resetBoard();
             });
-        }
     }
 }
 
@@ -356,97 +205,78 @@ function drawArena() {
     ctx.stroke();
 }
 
-function drawPuck() {
-    if (puckImg.complete && puckImg.naturalWidth > 0) {
-        ctx.drawImage(puckImg, x - r, y - r, r * 2, r * 2);
-    }
+function drawPaddle() {
+    ctx.drawImage(paddleImg, paddlex, HEIGHT - paddleh - 10, paddlew, paddleh);
 }
 
-function drawPaddle() {
-    if (paddleImg.complete && paddleImg.naturalWidth > 0) {
-        var py = HEIGHT - paddleh - 10;
-        ctx.drawImage(paddleImg, paddlex, py, paddlew, paddleh);
-    }
+function drawPuck() {
+    ctx.drawImage(puckImg, x - r, y - r, r * 2, r * 2);
 }
 
 function drawBricks() {
     for (var i = 0; i < NROWS; i++) {
         for (var j = 0; j < NCOLS; j++) {
-            if (bricks[i][j] > 0) {
-                var brickX = (j * (BRICKWIDTH + PADDING)) + BRICKOFFSETLEFT;
-                var brickY = (i * (BRICKHEIGHT + PADDING)) + BRICKOFFSETTOP;
+            if (bricks[i][j] <= 0) continue;
 
-                var brickGradient = ctx.createLinearGradient(brickX, brickY, brickX, brickY + BRICKHEIGHT);
-                brickGradient.addColorStop(0, "#274b74");
-                brickGradient.addColorStop(1, "#173150");
+            var bx = (j * (BRICKWIDTH + PADDING)) + BRICKOFFSETLEFT;
+            var by = (i * (BRICKHEIGHT + PADDING)) + BRICKOFFSETTOP;
 
-                ctx.fillStyle = brickGradient;
-                rect(brickX, brickY, BRICKWIDTH, BRICKHEIGHT);
+            var g = ctx.createLinearGradient(bx, by, bx, by + BRICKHEIGHT);
+            g.addColorStop(0, "#274b74");
+            g.addColorStop(1, "#173150");
 
-                ctx.lineWidth = 2;
-                ctx.strokeStyle = "#d9f3ff";
-                ctx.strokeRect(brickX, brickY, BRICKWIDTH, BRICKHEIGHT);
+            ctx.fillStyle = g;
+            ctx.beginPath();
+            if (ctx.roundRect) ctx.roundRect(bx, by, BRICKWIDTH, BRICKHEIGHT, 6);
+            else ctx.rect(bx, by, BRICKWIDTH, BRICKHEIGHT);
+            ctx.closePath();
+            ctx.fill();
 
-                ctx.fillStyle = "rgba(255,255,255,0.30)";
-                ctx.fillRect(brickX + 8, brickY + BRICKHEIGHT / 2 - 1, BRICKWIDTH - 16, 2);
-            }
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "#d9f3ff";
+            ctx.strokeRect(bx, by, BRICKWIDTH, BRICKHEIGHT);
+
+            ctx.fillStyle = "rgba(255,255,255,0.30)";
+            ctx.fillRect(bx + 8, by + BRICKHEIGHT / 2 - 1, BRICKWIDTH - 16, 2);
         }
     }
 }
 
-function collisionWithBricks() {
+function hitBrick() {
     for (var i = 0; i < NROWS; i++) {
         for (var j = 0; j < NCOLS; j++) {
-            if (bricks[i][j] > 0) {
-                var brickX = (j * (BRICKWIDTH + PADDING)) + BRICKOFFSETLEFT;
-                var brickY = (i * (BRICKHEIGHT + PADDING)) + BRICKOFFSETTOP;
+            if (bricks[i][j] <= 0) continue;
 
-                if (
-                    x + r > brickX &&
-                    x - r < brickX + BRICKWIDTH &&
-                    y + r > brickY &&
-                    y - r < brickY + BRICKHEIGHT
-                ) {
-                    dy = -dy;
-                    bricks[i][j] = 0;
-                    tocke++;
-                    updateScoreDisplay();
-                    preveriZmago();
-                    return;
-                }
+            var bx = (j * (BRICKWIDTH + PADDING)) + BRICKOFFSETLEFT;
+            var by = (i * (BRICKHEIGHT + PADDING)) + BRICKOFFSETTOP;
+
+            if (x + r > bx && x - r < bx + BRICKWIDTH && y + r > by && y - r < by + BRICKHEIGHT) {
+                dy = -dy;
+                bricks[i][j] = 0;
+                score++;
+                updateUI();
+                nextLevelCheck();
+                return;
             }
         }
     }
 }
 
 function draw() {
-    clear();
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
     drawArena();
 
-    if (rightDown) {
-        if ((paddlex + paddlew) < WIDTH) {
-            paddlex += 7;
-        } else {
-            paddlex = WIDTH - paddlew;
-        }
-    } else if (leftDown) {
-        if (paddlex > 0) {
-            paddlex -= 7;
-        } else {
-            paddlex = 0;
-        }
-    }
+    if (rightDown) paddlex = Math.min(WIDTH - paddlew, paddlex + 7);
+    if (leftDown) paddlex = Math.max(0, paddlex - 7);
 
     drawBricks();
     drawPaddle();
     drawPuck();
-    collisionWithBricks();
+    hitBrick();
 
-    if (x + dx > WIDTH - r || x + dx < 0 + r) {
-        dx = -dx;
-    }
+    if (x + dx > WIDTH - r || x + dx < r) dx = -dx;
 
-    if (y + dy < 0 + r) {
+    if (y + dy < r) {
         dy = -dy;
     } else {
         var paddleTop = HEIGHT - paddleh - 10;
@@ -459,28 +289,9 @@ function draw() {
         ) {
             dy = -Math.abs(dy);
             dx = 8 * ((x - (paddlex + paddlew / 2)) / paddlew);
-            start = true;
         } else if (y + dy > HEIGHT - r) {
-            lives--;
-            updateLivesDisplay();
-
-            if (lives <= 0) {
-                stopLoops();
-
-                showGameAlert(
-                    "Game Over",
-                    "You lost all lives.",
-                    "error",
-                    "Play Again"
-                ).then(function () {
-                    resetBoard();
-                });
-
-                return;
-            }
-
-            resetBall();
-            init_paddle();
+            resetBoard();
+            return;
         }
     }
 
@@ -490,90 +301,85 @@ function draw() {
 
 function pauseGame() {
     if (!gameStarted || gameWon || isPaused) return;
-
     isPaused = true;
     stopLoops();
-    updateActionButton();
+    updateUI();
 }
 
 function resumeGame() {
     if (!gameStarted || gameWon || !isPaused) return;
-
     isPaused = false;
-    updateActionButton();
+    updateUI();
     startLoops();
 }
 
 function actionButtonClick() {
-    if (!gameStarted) {
-        startNewGame();
-        return;
-    }
+    if (!gameStarted) startNewGame();
+    else if (isPaused) resumeGame();
+    else pauseGame();
+}
 
-    if (isPaused) {
-        resumeGame();
-    } else {
-        pauseGame();
-    }
+function init() {
+    stopLoops();
+    resetBall();
+    initPaddle();
+    initBricks();
+    seconds = 0;
+    score = 0;
+    gameWon = false;
+    isPaused = false;
+    gameStarted = true;
+    updateUI();
+    draw();
+    startLoops();
+}
+
+function startNewGame() {
+    level = 1;
+    init();
 }
 
 function resetBoard() {
     stopLoops();
-
-    ctx = $("#canvas")[0].getContext("2d");
-    WIDTH = $("#canvas").width();
-    HEIGHT = $("#canvas").height();
-
-    sekunde = 0;
-    izpisTimer = "00:00";
-    tocke = 0;
-    lives = 3;
+    seconds = 0;
+    score = 0;
     level = 1;
     gameWon = false;
-    start = true;
     isPaused = false;
     gameStarted = false;
-
-    $("#cas").html(izpisTimer);
-    updateScoreDisplay();
-    updateLivesDisplay();
-    $("#level").html(level);
-    updateActionButton();
-
     resetBall();
-    init_paddle();
-    initbricks();
+    initPaddle();
+    initBricks();
+    updateUI();
     draw();
 }
 
-$(document).ready(function () {
-    ctx = $("#canvas")[0].getContext("2d");
-    WIDTH = $("#canvas").width();
-    HEIGHT = $("#canvas").height();
-
-    if (!controlsAdded) {
-        controlsAdded = true;
-        $(document).keydown(onKeyDown);
-        $(document).keyup(onKeyUp);
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        rightDown = true;
+        e.preventDefault();
     }
-
-    $("#actionBtn").on("click", function () {
-        if (paddleImg.complete && paddleImg.naturalWidth > 0 && puckImg.complete && puckImg.naturalWidth > 0) {
-            actionButtonClick();
-            this.blur();
-        } else {
-            showGameAlert(
-                "Images Not Loaded",
-                "Check paddle.png and puck.png.",
-                "warning",
-                "OK"
-            );
-        }
-    });
-
-    if (imagesReady >= 2) {
-        resetBoard();
-    } else {
-        drawArena();
+    if (e.key === "ArrowLeft") {
+        leftDown = true;
+        e.preventDefault();
     }
 });
+
+document.addEventListener("keyup", function (e) {
+    if (e.key === "ArrowRight") {
+        rightDown = false;
+        e.preventDefault();
+    }
+    if (e.key === "ArrowLeft") {
+        leftDown = false;
+        e.preventDefault();
+    }
+});
+
+actionBtn.addEventListener("click", function () {
+    actionButtonClick();
+    this.blur();
+});
+
+drawArena();
+updateUI();
